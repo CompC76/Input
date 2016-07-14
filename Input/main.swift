86e20c071509f372ae82582244a8287dcdb005c0
Josh Birnholz <CompC@Josh-Birnholzs-MacBook-Pro.local>
2016-07-14 11:05:03 -0400

    Initial commit

---
+++ Input/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Input
+//
+//  Created by Josh Birnholz on 7/14/16.
+//  Copyright © 2016 Josh Birnholz. All rights reserved.
+//
+
+import Foundation
+
+print("What's your name?")
+
+let name = Input.next()
+
+print("Hello, \(name)!")
